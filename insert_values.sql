43c8e85ccf95eb2b7dfd1d5a14bbb41e3ccdd65d
LAPTOP-58K9CVVB\Magda <magdatrzcinska23@gmail.com>
2021-10-25 20:46:55 +0200

    inserting values into the customer table

--- insert_values.sql
+++ insert_values.sql
@@ -57,3 +57,72 @@ VALUES (19,'Emil','Kurowski','99020933396','Regular Specialist','Gdańsk');
 
 INSERT INTO employee (ID, name, surname, pesel, position_job, department)
 VALUES (20,'Rozalia','Jasińska','72090207024','Senior Specialist','Gdańsk');
+
+
+/* INSERT INTO CUSTOMER */
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(1, 'Gabriel', 'Kania', '20011594813', '+48878947546', 'Bydgoszcz');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(2, 'Wiesław', 'Piątek', '60021775710', '+48956032509', 'Zielona Góra');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(3, 'Tatiana', 'Pająk', '00240742322', '+48678045678', 'Sopot');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(4, 'Maksymilian', 'Nowakowski', '99231232350', '+48876047365', 'Świętochłowice');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(5, 'Monika', 'Marciniak', '75121104867', '+48986047222', 'Żory');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(6, 'Józef', 'Przybylski', '78021506551', '+48999578376', 'Legnica');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(7, 'Marcel', 'Marek', '69121253232', '+48888364777', 'Gdynia');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(8, 'Władysława', 'Krajewska', '89060973084', '+48986476009', 'Przemyśl');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(9, 'Bogumiła', 'Wrona', '72040477847', '+48957460987', 'Koszalin');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(10, 'Jakub', 'Klimek', '95040899958', '+48777096546', 'Suwałki');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(11, 'Sebastian', 'Brzeziński', '59010801310', '+48789573650', 'Sopot');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(12, 'Denis', 'Wysocki', '81111625258', '+48657460987', 'Kalisz');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(13, 'Martyna', 'Bukowska', '76042835144', '+48777895760', 'Ruda Śląska');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(14, 'Norbert', 'Kucharski', '49112304075', '+48657809564', 'Sopot');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(15, 'Julianna', 'Kowalska', '71082485249', '+48678058765', 'Leszno');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(16, 'Kuba', 'Kowalski', '77022868912', '+48987054765', 'Leszno');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(17, 'Józefa', 'Chojnacka', '74062229509', '+48768095765', 'Świnoujście');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(18, 'Antonina', 'Matusiak', '86050641769', '+48765984090', 'Kalisz');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(19, 'Kaja', 'Karpińska', '84110999423', '+48777890987', 'Gdańsk');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(20, 'Ksawery', 'Żak', '96072925538', '+48909876567', 'Rybnik');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(21, 'Józef', 'Małecki', '58031192986', '+48987543123', 'Krosno');
+
+INSERT INTO customer(ID, name, surname, pesel, phone, city)
+VALUES(22, 'Wiktor', 'Laskowski', '78040808612', '+48909543879', 'Gliwice');
