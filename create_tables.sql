febe8ac8975472f2da2df833a48387064189222c
LAPTOP-58K9CVVB\Magda <magdatrzcinska23@gmail.com>
2021-10-28 19:42:28 +0200

    create the contract_type table

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,4 @@
-create table employee (
+CREATE TABLE employee (
 ID int not null primary key,
 name varchar(20) not null,
 surname varchar(60) not null,
@@ -7,7 +7,7 @@ position_job varchar(70) not null,
 department varchar(10) not null
 );
 
-create table customer (
+CREATE TABLE customer (
 ID int not null primary key,
 name varchar(20) not null,
 surname varchar(60) not null,
@@ -16,7 +16,7 @@ phone varchar(12) not null check(phone REGEXP '[+][0-9]{11}'),
 city varchar(20) not null
 );
 
-create table car (
+CREATE TABLE car (
 ID int not null primary key,
 brand varchar(20) not null,
 country varchar(20) not null,
@@ -24,3 +24,7 @@ KM int not null,
 price_per_day int not null
 );
 
+CREATE TABLE contract_type(
+shortcut varchar(3) not null primary key,
+full_name varchar(40) not null
+);
